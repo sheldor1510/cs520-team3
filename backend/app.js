9c52ed3759d5db5b57262c5b4e79a45882e2e253
Anshul Saha <anshulsaha15@gmail.com>
2025-05-10 14:31:26 -0400

    integrated mongodb + made schemas for User and Interaction models

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,26 @@
-const express    = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const cors       = require('cors');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
+
+const mongoURI = process.env.DB_URI
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log("MongoDB connected.")
+    });
+
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.json({ message: 'Hello from your backened!' }));
-
+app.get('/', require('./routes/index.js'));
 app.use((req, res) => res.status(404).json({ error: 'Not Found' }));
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -18,5 +28,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Backened running on http://localhost:${PORT}`);
+  console.log(`Backend running on http://localhost:${PORT}`);
 });
