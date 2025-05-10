1d6e8b090097934d0766d6651434e5ec2314f3c4
Tanush Savadi <tanushsavadi@vl965-172-31-125-142.wireless.umass.edu>
2025-05-10 16:17:31 -0400

    fixed issue to use all endpoints from index.js file

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', require('./routes/index.js'));
+app.use('/', require('./routes/index.js'));
 app.use((req, res) => res.status(404).json({ error: 'Not Found' }));
 app.use((err, req, res, next) => {
   console.error(err.stack);
